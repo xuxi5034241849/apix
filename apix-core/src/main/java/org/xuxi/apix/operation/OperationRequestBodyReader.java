0ae32ab8e69a3a3bd1d333bbfd7841dc5dfce60f
xuchao <xuchao@wondersgroup.com>
2018-11-17 16:34:10 +0800

    逻辑确定

--- apix-core/src/main/java/org/xuxi/apix/operation/OperationRequestBodyReader.java
+++ apix-core/src/main/java/org/xuxi/apix/operation/OperationRequestBodyReader.java
@@ -9,8 +9,50 @@ import org.xuxi.apix.context.OperationContext;
 /**
  * 处理 RequestMapping 的 requestBody
  * 处理非基础类型&String
+ * <p>
+ * 处理这几种情况
+ * 所有参与计算的对象，必须被 @RequestBody标注
+ * <p>
+ * <p>
+ * 1、@Valid ：
+ * ApiModelProperty (选填)
+ * Notnull（必填）
+ * <p>
+ * 2、@Validate(g1) ：
+ * ApiModelProperty/ApiModelProperty(g1)/(空)/*ApiModelProperty(g2)*，Notnull(g1)（必填）
+ * ApiModelProperty(g1)，Notnull/ Notnull(g2)/(空)(选填)
+ * <p>
+ * <p>
+ * 3、@ApiGroup(g1) ：
+ * ApiModelProperty(g1)，NotNull/(空) (选填)
+ * <p>
+ * <p>
+ * 4、@Validate(g1) ,@ApiGroup(g1):
+ * ApiModelProperty/(空)/*ApiModelProperty(g2)*，Notnull(g1)（必填）
+ * ApiModelProperty(g1)，Notnull/(空)/ Notnull(g2)(选填)
+ * <p>
+ * <p>
+ * 5、@Validate(g1) ,@ApiGroup(g2):
+ * ApiModelProperty(g1)（选填）
+ * ApiModelProperty(g2)（选填）
+ * Notnull(g1)（必填）
+ * ApiModelProperty，Notnull(g1)（必填）*4.1
+ * ApiModelProperty(g1)，Notnull（选填）*4.2
+ * ApiModelProperty(g2)，Notnull（选填）
+ * ApiModelProperty(g1)，Notnull(g1) （必填）
+ * ApiModelProperty(g2)，Notnull(g2) （选填）
+ * ApiModelProperty(g1)，Notnull(g2) （选填）*4.2
+ * ApiModelProperty(g2)，Notnull(g1) （必填）*5.7
+ * <p>
+ * 思路：">":优先级      "-->":决定
+ * 0、vaild
+ * 1、Validate>ApiGroup
+ * 2、Validate-->Notnull （1、匹配成功，必填，且后续操作忽略）
+ *            -->ApiModelProperty （2、验证匹配成功，选填）
  *
- * @see @ReqyestParam 包含必填验证
+ * ApiGroup-->ApiModelProperty （3、Validate完全匹配失败，尝试匹配，如果成功：选填）
+ *
+ * @see {@ReqyestParam} 包含必填验证
  */
 @Component
 @Order(Ordered.OperationRequestBodyReader)
@@ -38,74 +80,21 @@ public class OperationRequestBodyReader implements OperationBuilderPlugin {
         if (HttpMethod.POST.equals(context.httpMethod())) {
 
 
-            /**
-             * 处理这几种情况
-             * 所有参与计算的对象，必须被 @RequestBody标注
-             *
-             *
-             * 1、@Valid ：
-             *      ApiModelProperty (选填)
-             *      Notnull（必填）
-             *      其他不显示
-             *
-             * 2、@Validate(g1) ：
-             *      ApiModelProperty/ApiModelProperty(g1)/(空)/*ApiModelProperty(g2)*，Notnull(g1)（必填）
-             *      ApiModelProperty(g1)，Notnull/ Notnull(g2)/(空)(选填)
-             *
-             *      ApiModelProperty，Notnull （不显示）
-             *      ApiModelProperty(g2)，Notnull(g2) （不显示）
-             *
-             * 3、@ApiGroup(g1) ：
-             *      ApiModelProperty(g1) (选填)
-             *      ApiModelProperty(g1)，NotNull (选填)
-             *
-             *      NotNull (不显示)
-             *      NotNull(g1) (不显示)
-             *      ApiModelProperty (不显示)
-             *      ApiModelProperty(g2) (不显示)
-             *
-             * 4、@Validate(g1) ,@ApiGroup(g1):
-             *      ApiModelProperty/(空)/*ApiModelProperty(g2)*，Notnull(g1)（必填）
-             *      ApiModelProperty(g1)，Notnull/(空)/ Notnull(g2)(选填)
-             *
-             *      ApiModelProperty，Notnull （不显示）
-             *      ApiModelProperty(g2)（不显示）
-             *      Notnull(g2) （不显示）
-             *      ApiModelProperty(g2)，Notnull(g2) （不显示）
-             *
-             * 5、@Validate(g1) ,@ApiGroup(g2):
-             *      ApiModelProperty(g1)（选填）
-             *      ApiModelProperty(g2)（选填）
-             *      Notnull(g1)（必填）
-             *      *Notnull(g2)*（不显示）
-             *      ApiModelProperty，Notnull(g1)（必填）
-             *      *ApiModelProperty，Notnull(g2)*（不显示）
-             *      ApiModelProperty(g1)，Notnull（选填）
-             *      ApiModelProperty(g2)，Notnull（选填）
-             *      ApiModelProperty(g1)，Notnull(g1) （必填）
-             *      ApiModelProperty(g2)，Notnull(g2) （选填）
-             *      ApiModelProperty(g1)，Notnull(g2) （选填）
-             *      ApiModelProperty(g2)，Notnull(g1) （必填）
-             *
-             *      ApiModelProperty (不显示)
-             *      Notnull (不显示)
-             *
-             *
-             *      xuchao思路：">":优先级      "-->":决定
-             *          1、Validate>ApiGroup
-             *
-             *          2、Validate-->Notnull
-             *                     -->ApiModelProperty
-             *
-             *              ApiGroup-->ApiModelProperty
-             *
-             *
-             */
-
-
         }
 
 
     }
+
+
+    /**
+     * 决定 required 属性
+     */
+    private void decisionRequired(){
+
+
+        
+    }
+
+
 
 }
